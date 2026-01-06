591d7beb62129ea32d02747d969ea4b8eb3e6811
Mr-fuaaaadh <fuhadmk4@gmail.com>
2026-01-06 12:32:35 +0530

    bww

--- rooms/urls.py
+++ rooms/urls.py
@@ -3,9 +3,10 @@ from django.urls import path, include
 from .views import RoomViewSet, FacilityViewSet, RoomImageViewSet
 
 router = DefaultRouter()
-router.register(r'rooms', RoomViewSet, basename='room')
-router.register(r'facilities', FacilityViewSet, basename='facility')
-router.register(r"room-images", RoomImageViewSet, basename="roomimage")
+router.register("rooms", RoomViewSet, basename="rooms")
+router.register("facilities", FacilityViewSet, basename="facilities")
+router.register("room-images", RoomImageViewSet, basename="room-images")
+
 urlpatterns = [
-    path('', include(router.urls)),
+    path("", include(router.urls)),
 ]
